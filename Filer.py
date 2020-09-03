ce54b1918d2cccb8cbda56249e2bf9f2f9e476c4
Martin Schobert <martin@schobert.cc>
2020-09-03 21:27:02 +0200

    Code reformatted using black.

--- Filer.py
+++ Filer.py
@@ -10,7 +10,15 @@ from threading import Thread
 from random import randint
 from sys import stderr, exit
 
-from flask import Flask, render_template, jsonify, request, redirect, send_from_directory, g
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect,
+    send_from_directory,
+    g,
+)
 from flask_wtf.csrf import CSRFProtect, CSRFError
 from flask_dropzone import Dropzone
 from flask_babel import Babel, _, refresh
@@ -19,26 +27,26 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 ### start of config
-app.config["SECRET_KEY"] = 'None'
+app.config["SECRET_KEY"] = "None"
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
 
 app.config["DROPZONE_ALLOWED_FILE_CUSTOM"] = True
 app.config["DROPZONE_ALLOWED_FILE_TYPE"] = ""
-app.config['DROPZONE_SERVE_LOCAL'] = True
-app.config['DROPZONE_ENABLE_CSRF'] = True
+app.config["DROPZONE_SERVE_LOCAL"] = True
+app.config["DROPZONE_ENABLE_CSRF"] = True
 
-app.config['ORGANIZATION'] = 'Kanzlei Hubrig'
-app.config['TITLE'] = "Filer"
-app.config['LANGUAGES'] = ['en', 'de']
+app.config["ORGANIZATION"] = "Kanzlei Hubrig"
+app.config["TITLE"] = "Filer"
+app.config["LANGUAGES"] = ["en", "de"]
 
-filettl = int(getenv('FILER_FILETTL', 10)) # file lifetime in days
+filettl = int(getenv("FILER_FILETTL", 10))  # file lifetime in days
 support_public_docs = True
 
-gpg_enable_upload_encryption = True # encrypt customer-uploaded data via GPG
+gpg_enable_upload_encryption = True  # encrypt customer-uploaded data via GPG
 gpg_recipient_fprint = None
-gpg_key_server='keys.openpgp.org'
+gpg_key_server = "keys.openpgp.org"
 
 basedir = getenv("FILER_BASEDIR", "./Daten")
 publicdir = getenv("FILER_PUBLICDIR", "Public")
@@ -53,51 +61,70 @@ dropzone = Dropzone(app)
 babel = Babel(app)
 
 
-nonce = base64.b64encode(urandom(64)).decode('utf8')
-default_http_header = \
-    {'Content-Security-Policy' : f"default-src 'self'; img-src 'self' data:; script-src 'self' 'nonce-{nonce}'",
-     'X-Frame-Options' : 'SAMEORIGIN',
-     'X-Content-Type-Options' : 'nosniff'}
+nonce = base64.b64encode(urandom(64)).decode("utf8")
+default_http_header = {
+    "Content-Security-Policy": f"default-src 'self'; img-src 'self' data:; script-src 'self' 'nonce-{nonce}'",
+    "X-Frame-Options": "SAMEORIGIN",
+    "X-Content-Type-Options": "nosniff",
+}
 
 
 def update_dropzone_message():
-    app.config[
-        "DROPZONE_DEFAULT_MESSAGE"
-    ] = _("Ziehe die Dateien hier hin, um sie hochzuladen oder klicken Sie zur Auswahl.")
+    app.config["DROPZONE_DEFAULT_MESSAGE"] = _(
+        "Ziehe die Dateien hier hin, um sie hochzuladen oder klicken Sie zur Auswahl."
+    )
+
 
 #### ADMIN FACING DIRECTORY LISTS ####
 ####
 ####
 @app.route("/admin", methods=["GET"])
 def admin():
-        
+
     update_dropzone_message()
-    url_root = request.url_root.replace('http://', 'https://', 1)
+    url_root = request.url_root.replace("http://", "https://", 1)
     users = listdir(path.join(basedir, clientsdir))
-    return render_template('admin.html', users = users, tree = make_tree(basedir, publicdir),
-                           url_root = url_root,
-			   documentsdir=documentsdir,
-			   support_public_docs=support_public_docs,
-                           nonce = nonce,
-                           organization = app.config['ORGANIZATION'],
-                           title = app.config['TITLE']), 200, default_http_header
-    
+    return (
+        render_template(
+            "admin.html",
+            users=users,
+            tree=make_tree(basedir, publicdir),
+            url_root=url_root,
+            documentsdir=documentsdir,
+            support_public_docs=support_public_docs,
+            nonce=nonce,
+            organization=app.config["ORGANIZATION"],
+            title=app.config["TITLE"],
+        ),
+        200,
+        default_http_header,
+    )
 
 
 @app.route("/admin/" + documentsdir + "/<user>", methods=["GET"])
 def admin_dokumente(user):
     update_dropzone_message()
-    return render_template('mandant.html', admin = 'admin/', user = secure_filename(user),
-                           tree = make_tree(basedir, path.join(documentsdir, secure_filename(user))),
-                           documentsdir=documentsdir,
-                           support_public_docs = support_public_docs,
-                           nonce = nonce,
-                           organization = app.config['ORGANIZATION'],
-                           title = app.config['TITLE']), 200, default_http_header
+    return (
+        render_template(
+            "mandant.html",
+            admin="admin/",
+            user=secure_filename(user),
+            tree=make_tree(basedir, path.join(documentsdir, secure_filename(user))),
+            documentsdir=documentsdir,
+            support_public_docs=support_public_docs,
+            nonce=nonce,
+            organization=app.config["ORGANIZATION"],
+            title=app.config["TITLE"],
+        ),
+        200,
+        default_http_header,
+    )
+
 
 #
 # API
 #
+
 
 @app.route("/admin/del-user/<user>", methods=["POST"])
 def admin_deluser(user):
@@ -125,9 +152,11 @@ def admin_newuser():
 
     try:
         make_dir(path.join(basedir, documentsdir, directory))
-        with open(path.join(basedir, clientsdir, directory), 'w+', encoding='utf-8') as htpasswd:
+        with open(
+            path.join(basedir, clientsdir, directory), "w+", encoding="utf-8"
+        ) as htpasswd:
             htpasswd.write("{}:{}\n".format(secure_filename(user), tagged_digest_salt))
-        
+
     except OSError as error:
         return "Couldn't create user scope", 500
     return redirect("/admin")
@@ -139,27 +168,42 @@ def admin_newuser():
 @app.route("/" + documentsdir + "/<user>", methods=["GET"])
 def mandant(user):
     update_dropzone_message()
-    return render_template('mandant.html', admin = '', user = secure_filename(user),
-                           tree = make_tree(basedir, path.join(documentsdir, secure_filename(user))),
-                           documentsdir=documentsdir,
-                           support_public_docs = support_public_docs,
-                           nonce = nonce,
-                           organization = app.config['ORGANIZATION'],
-                           title = app.config['TITLE']), 200, default_http_header
+    return (
+        render_template(
+            "mandant.html",
+            admin="",
+            user=secure_filename(user),
+            tree=make_tree(basedir, path.join(documentsdir, secure_filename(user))),
+            documentsdir=documentsdir,
+            support_public_docs=support_public_docs,
+            nonce=nonce,
+            organization=app.config["ORGANIZATION"],
+            title=app.config["TITLE"],
+        ),
+        200,
+        default_http_header,
+    )
+
 
 #### UPLOAD FILE ROUTES ####
 ####
 ####
 
+
 @app.route("/" + documentsdir + "/<user>", methods=["POST"])
 def upload_mandant_as_mandant(user):
-    return _upload_mandant(user, encrypt=(gpg_enable_upload_encryption and gpg_recipient_fprint is not None))
+    return _upload_mandant(
+        user,
+        encrypt=(gpg_enable_upload_encryption and gpg_recipient_fprint is not None),
+    )
+
 
 @app.route("/admin/" + documentsdir + "/<user>", methods=["POST"])
 def upload_mandant_as_admin(user):
     return _upload_mandant(user)
 
-@app.route('/admin', methods=['POST'])
+
+@app.route("/admin", methods=["POST"])
 def upload_admin():
     return _upload_mandant()
 
@@ -173,37 +217,47 @@ def _upload_mandant(user=None, encrypt=False):
                 pathname = path.join(basedir, documentsdir, username, filename)
 
                 if encrypt:
-                    pathname += '.gpg'
+                    pathname += ".gpg"
                     enc = gpgencryption.GPGEncryption(gpg_home_dir, gpg_key_server)
-                    enc.encrypt_fh(gpg_recipient_fprint, f, pathname) # no signing
-                    
+                    enc.encrypt_fh(gpg_recipient_fprint, f, pathname)  # no signing
+
                 else:
                     f.save(pathname)
             else:
                 f.save(path.join(basedir, publicdir, filename))
-    return 'upload template'
+    return "upload template"
+
 
 # handle CSRF error
 @app.errorhandler(CSRFError)
 def csrf_error(e):
     return e.description, 400
 
+
 #### DELETE FILE ROUTES ####
 ####
 ####
 @app.route("/" + documentsdir + "/<user>/<path:filename>", methods=["POST"])
 def delete_file_mandant(user, filename):
-    method = request.form.get('_method', 'POST')
-    if method == 'DELETE':
-        unlink(path.join(basedir, documentsdir, secure_filename(user), secure_filename(filename)))
+    method = request.form.get("_method", "POST")
+    if method == "DELETE":
+        unlink(
+            path.join(
+                basedir, documentsdir, secure_filename(user), secure_filename(filename)
+            )
+        )
     return redirect("/" + documentsdir + "/" + secure_filename(user))
 
 
 @app.route("/admin/" + documentsdir + "/<user>/<path:filename>", methods=["POST"])
 def delete_file_mandant_admin(user, filename):
-    method = request.form.get('_method', 'POST')
-    if method == 'DELETE':
-        unlink(path.join(basedir, documentsdir, secure_filename(user), secure_filename(filename)))
+    method = request.form.get("_method", "POST")
+    if method == "DELETE":
+        unlink(
+            path.join(
+                basedir, documentsdir, secure_filename(user), secure_filename(filename)
+            )
+        )
     return redirect("/admin/" + documentsdir + "/" + secure_filename(user))
 
 
@@ -231,7 +285,7 @@ def custom_static_public(filename):
     return send_from_directory(path.join(basedir, publicdir), filename)
 
 
-def make_tree(rel, pathname, clean_expired = True):
+def make_tree(rel, pathname, clean_expired=True):
     tree = dict(name=pathname, download=path.basename(pathname), children=[])
     try:
         lst = listdir(path.join(rel, pathname))
@@ -261,22 +315,24 @@ def cleaner_thread():
         # sleep for 6h plus jitter
         time.sleep(21600 + randint(1, 1800))
 
+
 def make_dir(dir_name):
     if not path.exists(dir_name):
         mkdir(dir_name)
         chmod(dir_name, 0o700)
 
+
 @babel.localeselector
 def get_locale():
-    if not g.get('lang_code', None):
-        g.lang_code = request.accept_languages.best_match(app.config['LANGUAGES'])
+    if not g.get("lang_code", None):
+        g.lang_code = request.accept_languages.best_match(app.config["LANGUAGES"])
     return g.lang_code
-        
+
 
 # Main program
 
 umask(0o177)
-        
+
 thread = Thread(target=cleaner_thread, args=())
 thread.daemon = True
 thread.start()
@@ -294,7 +350,7 @@ except:
     stderr.write("Error: Basedir not accessible\n")
     exit(1)
 
-if app.config['SECRET_KEY'] is None:
+if app.config["SECRET_KEY"] is None:
     stderr.write("Error: Flask secret key is not set.\n")
     exit(1)
 
@@ -302,7 +358,6 @@ if app.config['SECRET_KEY'] is None:
 if gpg_enable_upload_encryption and gpg_recipient_fprint is not None:
     enc = gpgencryption.GPGEncryption(gpg_home_dir, gpg_key_server)
     enc.download_key(gpg_recipient_fprint)
-
 
 
 if __name__ == "__main__":
--- gpgencryption.py
+++ gpgencryption.py
@@ -8,28 +8,35 @@ import os
 import sys
 import gnupg
 
+
 class GPGEncryption:
-    
-    def __init__(self, home_dir, key_server='keys.openpgp.org',
-                 sender_email_address=None, passphrase=None):
+    def __init__(
+        self,
+        home_dir,
+        key_server="keys.openpgp.org",
+        sender_email_address=None,
+        passphrase=None,
+    ):
 
         self.debug_print = True
         self.key_server = key_server
-        
+
         new_dir = self._check_and_create_homedir(home_dir)
-    
+
         self.gpg = gnupg.GPG(gnupghome=home_dir)
         if sender_email_address:
-            input_data = self.gpg.gen_key_input(name_email=sender_email_address,  passphrase=passphrase)
+            input_data = self.gpg.gen_key_input(
+                name_email=sender_email_address, passphrase=passphrase
+            )
             if new_dir:
-                # create a new key for signing                
+                # create a new key for signing
                 key = self.gpg.gen_key(input_data)
-                        
+
     def _debug(self, message):
-        if self.debug_print:            
-            #print(message, file=sys.stderr)
+        if self.debug_print:
+            # print(message, file=sys.stderr)
             print(message, file=sys.stdout)
-            
+
     def _check_and_create_homedir(self, home_dir):
         # if there is no directory, create one
         self._debug(f"+ Check home dir {home_dir}.")
@@ -42,11 +49,11 @@ class GPGEncryption:
 
     def _key_available(self, fingerprint):
         self._debug(f"+ Check if key {fingerprint} is locally available.")
-        
+
         # check if recipient key is in keystore
         public_keys = self.gpg.list_keys()
         for k in public_keys:
-            if k and k['fingerprint'] == fingerprint:
+            if k and k["fingerprint"] == fingerprint:
                 self._debug("+ Yes")
                 return k
         self._debug("+ No")
@@ -55,16 +62,18 @@ class GPGEncryption:
     def download_key(self, fingerprint, retries=3):
         if self._key_available(fingerprint):
             return True
-        
+
         for i in range(0, retries):
-            
+
             self._debug(f"+ Search for key {fingerprint} (retry #{i}).")
             found_keys = self.gpg.search_keys(fingerprint, self.key_server)
             self._debug(found_keys)
             for k in found_keys:
-                if k and k['keyid'] == fingerprint:
+                if k and k["keyid"] == fingerprint:
                     self._debug("+ Key found. Importing key.")
-                    return self.gpg.recv_keys(self.key_server, fingerprint) # returns an ImportResult                
+                    return self.gpg.recv_keys(
+                        self.key_server, fingerprint
+                    )  # returns an ImportResult
         self._debug("+ Key not found.")
         return False
 
@@ -72,69 +81,76 @@ class GPGEncryption:
         if not self._key_available(recipient_fingerprint):
             # search and download missing key
             if not self.download_key(recipient_fingerprint):
-                raise Exception('Recipient key is not available')
-            
+                raise Exception("Recipient key is not available")
+
             if not self._key_available(recipient_fingerprint):
                 # While we check the fprint on search result, there is no way to check the
                 # fprint on the receive result directly. Therefore, we just look at the
                 # fprint of the key in our keyring. If the right key is not yet there,
                 # we stop here.
-                raise Exception('Recipient key is not available')
+                raise Exception("Recipient key is not available")
 
         return True
-    
+
     def encrypt_file(self, recipient_fprint, filename_in, filename_out):
-        
+
         self._prepare_encryption(recipient_fprint)
-        
-        with open(filename_in, 'rb') as f:
+
+        with open(filename_in, "rb") as f:
             return self.encrypt_fh(recipient_fprint, f, filename_out)
-        
+
     def encrypt_fh(self, recipient_fprint, fhandle, filename_out):
-        
+
         self._prepare_encryption(recipient_fprint)
-        
+
         # For the downloaded key, we checked the fingerprint.
         # Therefore, we can trust the key.
-        status = self.gpg.encrypt_file(fhandle, recipients=[recipient_fprint],
-                                       output=filename_out, always_trust=True, armor=False)
-            
+        status = self.gpg.encrypt_file(
+            fhandle,
+            recipients=[recipient_fprint],
+            output=filename_out,
+            always_trust=True,
+            armor=False,
+        )
+
         if status.ok:
             return True
         else:
-            self._debug('ok: ', status.ok)
-            self._debug('status: ', status.status)
-            self._debug('stderr: ', status.stderr)
+            self._debug("ok: ", status.ok)
+            self._debug("status: ", status.status)
+            self._debug("stderr: ", status.stderr)
             raise Exception("Can't encrypt file")
-            
+
         return False
 
     def encrypt_text(self, recipient_fprint, text):
         self._prepare_encryption(recipient_fprint)
-        
-        status = self.gpg.encrypt(text, recipients=[recipient_fprint], always_trust=True)
-            
+
+        status = self.gpg.encrypt(
+            text, recipients=[recipient_fprint], always_trust=True
+        )
+
         if status.ok:
             return str(status)
         else:
-            self._debug('ok: ', status.ok)
-            self._debug('status: ', status.status)
-            self._debug('stderr: ', status.stderr)
+            self._debug("ok: ", status.ok)
+            self._debug("status: ", status.status)
+            self._debug("stderr: ", status.stderr)
             raise Exception("Can't encrypt text")
-        
+
         return None
-    
+
+
 if __name__ == "__main__":
-    home_dir = 'daten/gpg'
-    recipient_key_fprint = '900611B00B54591F720E5BF656F9EDB79BE4CD06'
-    passphrase = 'secret'
+    home_dir = "daten/gpg"
+    recipient_key_fprint = "900611B00B54591F720E5BF656F9EDB79BE4CD06"
+    passphrase = "secret"
 
     enc = GPGEncryption(home_dir)
     enc.download_key(recipient_key_fprint)
-    
-    #enc = GPGEncryption(home_dir, recipient_key_fprint, passphrase)
-    
-    #status = enc.encrypt_file(recipient_key_fprint, '/etc/passwd', '/tmp/passwd.gpg')
-    #cipher_text = enc.encrypt_text(recipient_key_fprint, 'hello')
-    #print(cipher_text)
-    
+
+    # enc = GPGEncryption(home_dir, recipient_key_fprint, passphrase)
+
+    # status = enc.encrypt_file(recipient_key_fprint, '/etc/passwd', '/tmp/passwd.gpg')
+    # cipher_text = enc.encrypt_text(recipient_key_fprint, 'hello')
+    # print(cipher_text)
